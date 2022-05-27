44402e0b67ed56d5c1a00780948c056dd354a571
un-earthly <vijayalamin@gmail.com>
2022-05-27 04:27:27 -0700

    live sites page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,41 @@
 import React from 'react'
 import Navbar from './Navbar'
-
 export default function Home() {
+  const workData = [
+
+    {
+      ss: "https://i.ibb.co/99dfcWW/image.png",
+      title: 100,
+      techonologies: 'stripe,react,Daisy ui,nodejs,Mongodb,Firebase,react-hooks-form,tailwindcss,',
+      live: "https://github.com/un-earthly/flying-wheels-client.git"
+    },
+    {
+      ss: "https://i.ibb.co/99dfcWW/image.png",
+      title: 100,
+      techonologies: 'stripe,react,Daisy ui,nodejs,Mongodb,Firebase,react-hooks-form,tailwindcss,',
+      live: "https://github.com/un-earthly/flying-wheels-client.git"
+    },
+    {
+      ss: "https://i.ibb.co/99dfcWW/image.png",
+      title: 100,
+      techonologies: 'stripe,react,Daisy ui,nodejs,Mongodb,Firebase,react-hooks-form,tailwindcss,',
+      live: "https://github.com/un-earthly/flying-wheels-client.git"
+    },
+    {
+      ss: "https://i.ibb.co/99dfcWW/image.png",
+      title: 100,
+      techonologies: 'stripe,react,Daisy ui,nodejs,Mongodb,Firebase,react-hooks-form,tailwindcss,',
+      live: "https://github.com/un-earthly/flying-wheels-client.git"
+    },
+    {
+      ss: "https://i.ibb.co/99dfcWW/image.png",
+      title: 100,
+      techonologies: 'stripe,react,Daisy ui,nodejs,Mongodb,Firebase,react-hooks-form,tailwindcss,',
+      live: "https://github.com/un-earthly/flying-wheels-client.git"
+    },
+
+
+  ]
   return (
     <div >
       <Navbar />
@@ -9,7 +43,7 @@ export default function Home() {
       {/* HOME SECTION */}
       <div data-home className="d-flex flex-column ps-5 justify-content-center align-items-center h-100" >
         <h1 data-heading>MD.ALAMIN</h1>
-        <p style={{ color: 'lightcoral' }} className='h6 my-4'>REACT JS | NODE JS | MONOGODB | JAVASCRIPT </p>
+        <p style={{ color: 'lightcoral' }} className='h6 my-4'> MONOGODB | EXPRESS JS | REACT JS | NODE JS  </p>
         <div >
           <a href="https://github.com/un-earthly" target="_blank" rel="noopener noreferrer"><i className="pe-4 text-secondary bi bi-github"></i></a>
           <a href="mailto:vijayalamin@gmail.com"><i className="px-4 text-secondary bi bi-envelope"></i></a>
@@ -18,9 +52,22 @@ export default function Home() {
       </div>
 
 
-      {/* SKILLS SECTION */}
+      {/* WORKS SECTION */}
 
-      
+
+      <div data-work className="container py-5" >
+        {
+          workData.map(w => (
+            <div class="card my-3" >
+              <div class="card-body d-flex align-items-center justify-content-center" style={{ backgroundSize: 'cover', minHeight: '500px', backgroundPosition: 'center center', backgroundImage: `url(${w.ss})` }}>
+                <p class="card-text">{w.techonologies}.</p>
+                <a href={w.live} class="card-link">{w.title}</a>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+
 
     </div>
   )
