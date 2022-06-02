3b89e2e207864db4da2691908c1c07d5a0290cc0
un-earthly <vijayalamin@gmail.com>
2022-06-02 04:06:56 -0700

    about with blog route

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import RequireAdmin from "./Components/RequireAdmin";
 import Message from "./Components/Message.js";
 import ManageSkills from "./Components/ManageSkills.js";
 import ManageProject from "./Components/ManageProject.js";
+// import Blogs from "./Components/Blogs";
+// import About from "./Components/About";
+// import Blog from "./Components/Blog";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Home />} />
+        {/* <Route path='/blogs' element={<Blogs />} />
+        <Route path='/blog/:name' element={<Blog />} />
+        <Route path='/About' element={<About />} /> */}
         <Route path="/admin" element={<RequireAdmin></RequireAdmin>}>
           <Route index element={<Message />} />
           <Route path="/admin/messages" element={<Message />} />
--- src/Components/FloatingNav.js
+++ src/Components/FloatingNav.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function FloatingNav() {
     return (
-        <div>FloatingNav</div>
+        <div data-nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/blogs">Blogs</Link>
+        </div>
     )
 }
