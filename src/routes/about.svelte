13373dc2a1e7782d94b2821a443cb8b32b4fee8f
un-earthly <vijayalamin@gmail.com>
2022-07-22 03:21:27 -0700

    about page draft

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,5 +1,29 @@
-<h1 class="text-8xl text-white">About</h1>
-<div class="h-screen" />
-<div class="h-screen">
-    <!-- <div class="text-8xl text-white">hello</div> -->
+<svelte:head>
+    <title>About - MD Alamin</title>
+</svelte:head>
+
+<div class="p-6 text-white w-3/4">
+    <h1 class="text-8xl">About</h1>
+    <div class="p-12">
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias
+            accusantium maxime tempora corrupti quaerat id laudantium voluptas,
+            suscipit molestiae nobis error expedita quod accusamus eaque dolorem
+            quas, eum ipsam cumque fuga culpa nulla mollitia pariatur qui. Unde
+            magni, adipisci beatae natus harum quos quasi repellat molestias
+            quaerat esse error maiores cupiditate! Sequi nihil ut magni nam
+            atque molestiae voluptas tempore expedita inventore nesciunt
+            repudiandae officiis explicabo amet eveniet, magnam, dicta saepe
+            delectus, ipsam deserunt libero facilis culpa similique fugit ipsa!
+            Inventore repellendus excepturi aliquid ratione? Accusamus sint
+            deserunt obcaecati, illo reiciendis unde! Cumque aut itaque sint
+            eveniet enim autem distinctio.
+        </p>
+        <div class="p-16">
+            <img
+                src="https://dummyimage.com/600x400/fff/000&text=hello"
+                alt=""
+            />
+        </div>
+    </div>
 </div>
