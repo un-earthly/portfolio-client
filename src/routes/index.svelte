7f4cf4644c4d199cb03bf51b34066d5f05e5cd50
un-earthly <vijayalamin@gmail.com>
2022-07-21 19:33:39 -0700

    basic setupwith tailwind and fonts

---
+++ src/routes/index.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+    <title>Home - MD Alamin</title>
+</svelte:head>
+<h1 class="text-3xl font-bold font-breathing">Hello world!</h1>
+<p class="font-azoSans font-bold text-5xl">What's upp!!</p>
+<p class="font-azoSans ">having fun, huh!!</p>
